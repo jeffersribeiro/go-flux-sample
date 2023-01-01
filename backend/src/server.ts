ee409228d2e0348ea7a72dbfb83719fe8519fc7b
Jefferson <jefferson_ribeiro12345@outlook.com>
2022-12-31 21:52:21 -0300

    chore: general changes

---
+++ backend/src/server.ts
@@ -0,0 +1,65 @@
+import "dotenv/config";
+import "reflect-metadata";
+import "express-async-errors";
+
+import cors from "cors";
+import morgan from "morgan";
+import express, { NextFunction, Request, Response } from "express";
+import { AppDataSource } from "./config/data-source";
+import router from "./routes";
+import { ZodError } from "zod";
+import { QueryFailedError } from "typeorm";
+import AppError from "./common/errors/AppError";
+
+const { PORT, NODE_ENV } = process.env;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(router);
+
+app.use(
+  (
+    error: Error,
+    _request: Request,
+    response: Response,
+    _next: NextFunction
+  ) => {
+    console.error({ error });
+
+    if (error instanceof AppError) {
+      return response.status(error.code).json({
+        error,
+      });
+    } else if (error instanceof ZodError) {
+      return response.status(422).json({
+        errors: error.issues,
+      });
+    } else if (error instanceof QueryFailedError) {
+      return response.status(400).json({
+        message: error.message,
+        name: error.name,
+        error,
+      });
+    }
+
+    return response.status(500).json({
+      error,
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+);
+
+app.listen(PORT, async () => {
+  try {
+    await AppDataSource.initialize();
+
+    console.log(`server started at PORT: ${PORT} | ENV: ${NODE_ENV}`);
+  } catch (error) {
+    console.error(error);
+  }
+});
