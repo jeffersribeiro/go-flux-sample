5e5a727894a6732e7426222b0cacb754f6cfd1bd
Jefferson <jefferson_ribeiro12345@outlook.com>
2023-01-01 17:57:09 -0300

    chore: add new entities

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -2,7 +2,10 @@ import { DataSource, EntityTarget, ObjectLiteral, Repository } from "typeorm";
 
 import { Post } from "@modules/post/entities/post.entity";
 import { User } from "@modules/user/entities/user.entity";
-import { Session } from "@data/modules/session/entities/session.entity";
+import { Order } from "@modules/order/entities/order.entity";
+import { Session } from "@modules/session/entities/session.entity";
+import { Product } from "@modules/product/entities/product.entity";
+import { Purchase } from "@modules/purchase/entities/purchase.entity";
 
 const {
   TYPEORM_CONNECTION,
@@ -23,7 +26,7 @@ export const AppDataSource = new DataSource({
   database: TYPEORM_DATABASE,
   synchronize: TYPEORM_SYNCHRONIZE,
   logging: false,
-  entities: [User, Post, Session],
+  entities: [User, Post, Session, Product, Purchase, Order],
   subscribers: [],
   migrations: [],
 });
