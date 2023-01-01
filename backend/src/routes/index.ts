d09f6d8a7cece3b3213ca69d6f6dc32436120200
Jefferson <jefferson_ribeiro12345@outlook.com>
2023-01-01 17:57:43 -0300

    chore: routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
 
-import userRouter from "@data/modules/user/routes/user.route";
-import postRouter from "@data/modules/post/routes/post.route";
-import sessionRouter from "@data/modules/session/routes/session.route";
+import userRouter from "@modules/user/routes/user.route";
+import postRouter from "@modules/post/routes/post.route";
+import sessionRouter from "@modules/session/routes/session.route";
+import productRouter from "@modules/product/routes/product.route";
+import orderRouter from "@data/modules/order/routes/order.route";
+import purchaseRouter from "@data/modules/purchase/routes/purchase.route";
 
-import authenticate from "@data/middlewares/auth.middleware";
 import { token } from "@data/middlewares/token.middleware";
+import { authenticate } from "@data/middlewares/auth.middleware";
+import { salesman } from "@data/modules/product/middlewares/salesman.middleware";
 
 const router = Router();
 
+router.use("/user", userRouter);
 router.use("/session", sessionRouter);
-router.use("/user", authenticate, token, userRouter);
 router.use("/post", authenticate, token, postRouter);
+router.use("/order", authenticate, token, orderRouter);
+router.use("/purchase", authenticate, token, purchaseRouter);
+router.use("/product", authenticate, token, salesman, productRouter);
 
 export default router;
