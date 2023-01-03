05f84156149562e07d1769d0b3388a26d5e420d6
Jefferson <jefferson_ribeiro12345@outlook.com>
2023-01-03 17:13:04 -0300

    chore: modify user interface

--- mobile/interfaces/User.ts
+++ mobile/interfaces/User.ts
@@ -1,4 +1,5 @@
 import Dates from "./Dates";
+import Product from "./Product";
 
 export enum Type {
   Salesman = "SALESMAN",
@@ -8,10 +9,15 @@ export enum Type {
 export default interface User extends Dates {
   id: number;
   type: Type;
+  avatar: string;
   email: string;
   lastName: string;
   username: string;
   password: string;
   firstName: string;
   isActive: boolean;
+}
+
+export interface Seller extends User {
+  products: Product[];
 }
