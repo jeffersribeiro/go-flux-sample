b467f4b54aac50baa2635f80d82962050a516002
Jefferson <jefferson_ribeiro12345@outlook.com>
2023-01-03 17:10:15 -0300

    chore: add session start validation

--- backend/src/modules/session/controllers/session.controller.ts
+++ backend/src/modules/session/controllers/session.controller.ts
@@ -11,7 +11,11 @@ export const login = async (
 
   if (Array.isArray(ip)) ip = ip[0];
 
-  const session = await services.login({ email, ip, password });
+  const session = await services.login({
+    email: email.toLowerCase(),
+    ip,
+    password,
+  });
 
   return res.status(200).json(session);
 };
--- backend/src/modules/session/routes/session.route.ts
+++ backend/src/modules/session/routes/session.route.ts
@@ -4,10 +4,12 @@ import { token } from "@data/middlewares/token.middleware";
 import { authenticate } from "@data/middlewares/auth.middleware";
 
 import * as controllers from "../controllers";
+import { validateSchema } from "@data/middlewares/validate.middleware";
+import { loginSchema } from "../validators/login.validator";
 
 const sessionRouter = Router();
 
-sessionRouter.post("/", controllers.login);
+sessionRouter.post("/", validateSchema(loginSchema), controllers.login);
 
 sessionRouter.get("/", authenticate, token, controllers.logout);
 
